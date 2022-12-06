f29427af8a076d88da9c7df6ddc8cd80cc33186c
Arjen Duinhouwer <arjen@arjenduinhouwer.nl>
2022-12-06 17:12:15 +0100

    solved part 1

--- day4/main.c
+++ day4/main.c
@@ -4,18 +4,28 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define FILENAME "../sample.txt"
-//#define FILENAME "../input.txt"
+//#define FILENAME "../sample.txt"
+#define FILENAME "../input.txt"
 
-int total_score = 0;
+int set[4] = {-1};
+
+void push(int value) {
+    for (size_t i = 0; i < sizeof(set)/4; ++i) {
+        if(set[i] <= 0) {
+            set[i] = value;
+            break;
+        }
+    }
+}
 
 size_t getInt(char line[], size_t s)
 {
     size_t end;
 
     for (int i = s; i < 100; ++i) {
-        if(line[i] == 44 || line[i] == 45) {
+        if(line[i] == 44 || line[i] == 45 || line[i] == '\0') {
            end = i;
+           break;
         }
     }
 
@@ -29,24 +39,60 @@ size_t getInt(char line[], size_t s)
     }
 
     int result = atoi(sub);
-//    printf("result = %d\n", result);
-
+    push(result);
     return end;
 
 }
 
-int handle_line(char line[])
+void calculate_result_for_line(int *score) {
+    if((set[0] <= set[2] && set[1] >= set[3]) || (set[0] >= set[2] && set[1] <= set[3])) {
+        *score += 1;
+        //printf("%d - %d, %d - %d\n", set[0], set[1], set[2], set[3]);
+        //printf("Score: %d\n", *score);
+    }
+}
+void clear_set(void)
 {
-    size_t length = strlen(line)-1;
+    for (size_t i = 0; i < sizeof(set); ++i) {
+        set[i] = -1;
+    }
+}
 
-    for (size_t i = 0; i < length; ++i) {
-        if(line[i] != 44 && line[i] != 45) {
-            i = getInt(line, i)+1;
-            printf("%zu\n", i);
-        }
+void split(char line[])
+{
+    char *p;
+    char *p1, *p2;
+    p = strtok(line, ",");
+    if(p) {
+        p1 = p;
+        p = strtok(NULL, ",");
+        p2 = p;
     }
 
-    return 0;
+    char *s;
+
+    s = strtok(p1, "-");
+
+    if(s) {
+        set[0] = atoi(s);
+        s = strtok(NULL, "-");
+        set[1] = atoi(s);
+    }
+
+    char *s2;
+    s2 = strtok(p2, "-");
+    if(s2) {
+        set[2] = atoi(s2);
+        s2 = strtok(NULL, "-");
+        set[3] = atoi(s2);
+    }
+}
+
+void handle_line(char line[], int *score)
+{
+    split(line);
+    calculate_result_for_line(score);
+    clear_set();
 }
 
 void read_file()
@@ -59,18 +105,21 @@ void read_file()
 
     char line[256];
 
+    int score = 0;
     while(fgets(line, sizeof(line), f)) {
         if(strlen(line) == 1) {
             printf("End\n");
         }
 
-        total_score += handle_line(line);
+        handle_line(line, &score);
     }
+    printf("Score: %d\n", score);
     fclose(f);
 }
 
-int main(void) {
+int main(void)
+{
     read_file();
-    printf("Total score: %d\n", total_score);
+//    printf("Total score: %d\n", *total_score);
     return 0;
 }
