7d3aa1df559e47bdbce24d6a67ab864cfaf5c7c0
Arjen Duinhouwer <arjen@arjenduinhouwer.nl>
2022-12-07 13:31:34 +0100

    Solved day 5 pt2

--- day5/main.c
+++ day5/main.c
@@ -5,27 +5,31 @@
 #include <stdbool.h>
 #include <ctype.h>
 
-#define FILENAME "../sample.txt"
-//#define FILENAME "../input.txt"
+//#define FILENAME "../sample.txt"
+#define FILENAME "../input.txt"
 
-char crates[3][10] = {{'Z', 'N'}, {'M', 'C', 'D'}, {'P'}};
-//char crates[9][1000] = {
-//        {'T', 'P', 'Z', 'C', 'S', 'L', 'Q', 'N'},
-//        {'L', 'P', 'T', 'V', 'H', 'C', 'g'},
-//        {'D', 'C', 'Z', 'F'},
-//        {'G', 'W', 'T', 'D', 'L', 'M', 'V', 'C'},
-//        {'P', 'W', 'C'},
-//        {'P', 'F', 'J', 'D', 'C', 'T', 'S', 'Z'},
-//        {'V', 'W', 'G', 'B', 'D'},
-//        {'N', 'J', 'S', 'Q', 'H', 'W'},
-//        {'R', 'C', 'Q', 'F', 'S', 'L', 'V'}
-//};
+#define COLS  9
+#define COL_CAPACITY 1000
+
+//char crates[COLS][COL_CAPACITY] = {{'Z', 'N'}, {'M', 'C', 'D'}, {'P'}};
+char crates[COLS][COL_CAPACITY] = {
+        {'T', 'P', 'Z', 'C', 'S', 'L', 'Q', 'N'},
+        {'L', 'P', 'T', 'V', 'H', 'C', 'G'},
+        {'D', 'C', 'Z', 'F'},
+        {'G', 'W', 'T', 'D', 'L', 'M', 'V', 'C'},
+        {'P', 'W', 'C'},
+        {'P', 'F', 'J', 'D', 'C', 'T', 'S', 'Z'},
+        {'V', 'W', 'G', 'B', 'D'},
+        {'N', 'J', 'S', 'Q', 'H', 'W'},
+        {'R', 'C', 'Q', 'F', 'S', 'L', 'V'}
+};
 
 void draw_stack() {
-    for (int i = 0; i < 9; ++i) {
-        for (int j = 0; j < 1000; ++j) {
-            if(isprint(crates[i][j]))
+    for (int i = 0; i < COLS; ++i) {
+        for (int j = 0; j < COL_CAPACITY; ++j) {
+            if(isprint(crates[i][j])) {
                 printf("[%c]", crates[i][j]);
+            } else {break;}
         }
         printf("\n");
     }
@@ -33,18 +37,11 @@ void draw_stack() {
 
 }
 
-void move_crate(size_t from, size_t to, size_t amount)
+void move_crate(size_t from, size_t to, size_t index)
 {
-    char to_move;
-    for(size_t i = 0; i < 1000; ++i) {
-        if(!isprint(crates[from][i+1])){
-            to_move = crates[from][i];
-            crates[from][i] = -1;
-            break;
-        }
-    }
+    char to_move = crates[from][index];
 
-    for (size_t j = 0; j < 1000; ++j) {
+    for (size_t j = 0; j < COL_CAPACITY; ++j) {
         if(!isprint(crates[to][j])){
             crates[to][j] = to_move;
             break;
@@ -52,10 +49,43 @@ void move_crate(size_t from, size_t to, size_t amount)
     }
 }
 
+void prepare_move_crate(size_t from, size_t to, size_t amount) {
+    size_t end;
+    for(size_t i = 0; i < COL_CAPACITY; ++i) {
+//        printf("%zu\n", i);
+        if(!isprint(crates[from][i+1])){
+            end = i;
+            break;
+        }
+    }
+
+    size_t start = end-(amount-1);
+//    printf("%zu -> %zu: %zu\n", start, end, amount);
+
+    if(amount == 1) {
+        move_crate(from, to, end);
+    } else {
+        for (size_t j = start; j <= end; ++j) {
+            printf("Move %zu from %zu to %zu vanaf index %zu\n", amount, from , to, j);
+            move_crate(from, to, j);
+        }
+    }
+
+    if(amount == 1) {
+        for (size_t i = end; i < COL_CAPACITY; ++i) {
+            crates[from][i] = -1;
+        }
+    } else {
+        for (size_t k = start; k < COL_CAPACITY; ++k) {
+            crates[from][k] = -1;
+        }
+    }
+
+    draw_stack();
+}
+
 void handle_line(char line[])
 {
-    //move, amount, from, src, dest
-//    printf("%s\n", line);
     char *p;
     char *current;
     size_t amount, src, dest = -1;
@@ -70,25 +100,18 @@ void handle_line(char line[])
         current = strtok(NULL, " ");
         dest = atoi(current);
     }
-
 //    printf("amount: %zu\n", amount);
 //    printf("src: %zu\n", src);
 //    printf("dest: %zu\n", dest);
 
-    for (size_t i = 0; i < amount; ++i) {
-        move_crate(src-1, dest-1);
-    }
+    prepare_move_crate(src-1, dest-1, amount);
 
-//    draw_stack();
 }
 
 void get_message()
 {
-    for (int i = 0; i < 9; ++i) {
-        for (int j = 0; j < 1000; ++j) {
-//            if(i = 2) {
-//                printf("%c\n", crates[i][j]);
-//            }
+    for (int i = 0; i < COLS; ++i) {
+        for (int j = 0; j < COL_CAPACITY; ++j) {
             if(!isprint(crates[i][j+1])) {
                 printf("%c", crates[i][j]);
                 break;
@@ -106,21 +129,23 @@ void read_file()
     }
 
     char line[256];
-
+    int cnt = 0;
     while(fgets(line, sizeof(line), f)) {
+//        if(cnt == 2)
+//            break;
+
         if(strlen(line) == 1) {
             printf("End\n");
         }
-
+//        cnt++;
         handle_line(line);
     }
-
     fclose(f);
 }
 
 int main(void)
 {
-//    draw_stack();
+    draw_stack();
 //    exit(1);
     read_file();
     draw_stack();
